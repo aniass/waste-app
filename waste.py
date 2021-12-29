b26d099e2bde881bf64e3f7d57fbf5f12ccec5c3
Aniass <a.staskiewicz4@gmail.com>
2021-12-29 19:44:06 +0100

    added clean review

--- waste.py
+++ waste.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-MODELSPATH = '.\models\waste_model.h5'
-DATAPATH = '.\data\waste1.jpg'
+MODELSPATH = '.\\models\\waste_model.h5'
+DATAPATH = '.\\data\\waste1.jpg'
 
 
 @st.cache
@@ -14,7 +14,7 @@ def load_data():
 
 
 def load_models():
-    model = load_model(MODELSPATH, compile = False)
+    model = load_model(MODELSPATH, compile=False)
     return model
 
 
@@ -26,41 +26,36 @@ st.sidebar.title('Select options:')
 page = st.sidebar.selectbox("Choose a page:", ["Sample Data", "Upload an Image"])
 
 if page == "Sample Data":
-     st.header("Sample prediction for waste")  
-     
-     if st.checkbox('Show Sample Data'):
-         st.info("Sample image:")
-         image = load_data()
-         st.image(image, caption='Sample Data', use_column_width=True)
-         image = image.resize((64,64))
-         image = tf.keras.preprocessing.image.img_to_array(image)
-         image = image.reshape((1,64,64,3))
-         
-         st.subheader("Check waste prediction")
-         if st.checkbox('Show Prediction of simple image'):
+    st.header("Sample prediction for waste")
+    if st.checkbox('Show Sample Data'):
+        st.info("Sample image:")
+        image = load_data()
+        st.image(image, caption='Sample Data', use_column_width=True)
+        image = image.resize((64, 64))
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = image.reshape((1, 64, 64, 3))
+        st.subheader("Check waste prediction")
+        if st.checkbox('Show Prediction of simple image'):
             model = load_models()
             result = model.predict(image)
             st.write(result)
             if result[0][0] == 1:
                 prediction = 'Recyclable Waste'
             else:
-                prediction = 'Organic Waste' 
+                prediction = 'Organic Waste'
             st.write(prediction)
             st.success("It is prediction for sample image!")
-     
 
 if page == "Upload an Image":
     st.header("Your prediction of waste")
-    
-    uploaded_file = st.file_uploader("Choose your image", type=["jpg","png"])
+    uploaded_file = st.file_uploader("Choose your image", type=["jpg", "png"])
     if uploaded_file is not None:
         img = Image.open(uploaded_file)
         st.info("Show your image:")
         st.image(img, caption="Upload image", use_column_width=True)
-        img = img.resize((64,64))
+        img = img.resize((64, 64))
         img = tf.keras.preprocessing.image.img_to_array(img)
-        img = img.reshape((1,64,64,3))
-        
+        img = img.reshape((1, 64, 64, 3))
         st.subheader("Check waste prediction")
         if st.checkbox('Show Prediction of your image'):
             model = load_models()
@@ -69,6 +64,6 @@ if page == "Upload an Image":
             if result[0][0] == 1:
                 prediction = 'Recyclable Waste'
             else:
-                prediction = 'Organic Waste' 
+                prediction = 'Organic Waste'
             st.write(prediction)
             st.success("That is your prediction!")
